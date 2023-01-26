5fde1d2da24bc0f340fe61335f2ffbb9f3d411c7
priyank <a.priyank95@gmail.com>
2023-01-26 16:03:50 +0530

    Move documentation to Demo.java file.

--- src/main/java/structural/flyweight/User.java
+++ src/main/java/structural/flyweight/User.java
@@ -6,17 +6,6 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-/**
- * Flyweight design pattern is a Structural design pattern like Facade pattern, Adapter Pattern and Decorator pattern.
- * Flyweight design pattern is used when we need to create a lot of Objects of a class.
- * Since every object consumes memory space that can be crucial for low memory devices,
- * such as mobile devices or embedded systems, flyweight design pattern can be applied to reduce the load on memory by
- * sharing objects. Before we apply flyweight design pattern, we need to consider following factors:
- * 1. The number of Objects to be created by application should be huge.
- * 2. The object creation is heavy on memory, and it can be time-consuming too.
- * 3. The object properties can be divided into intrinsic and extrinsic properties, extrinsic properties of an Object
- * should be defined by the client program.
- */
 public class User {
     private static List<String> nameList = new ArrayList<>();
     private int[] names;
